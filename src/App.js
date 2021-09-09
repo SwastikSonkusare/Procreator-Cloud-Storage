6f359832d4f301b6ed94edc53628d50f2f5cce28
Swastik <swastik.sonkusare18@gmail.com>
2021-09-09 18:43:54 +0530

    added: notification screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Home from "./components/Home/Home";
+import Notification from "./components/Notification/Notification";
 import OnBoarding from "./components/OnBoarding/onBoarding";
 
 const App = () => {
-  return <Home />;
+  return <Notification />;
 };
 
 export default App;

dbcbaac7b33d5d1d45a86e029ff9019a57ab1470
Swastik <swastik.sonkusare18@gmail.com>
2021-09-10 16:56:45 +0530

    added: sass variables

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -28,7 +28,7 @@ const Header = () => {
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
+          viewBox="30 0 1200 120"
           preserveAspectRatio="none"
         >
           <path
